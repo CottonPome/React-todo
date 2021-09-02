b1abc482542873b5bcc39e0501fa6c9ade1eaffb
SibaPome <p6aa73ik@s.okayama-u.ac.jp>
2021-09-03 00:14:31 +0900

    tutorial completed!

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 import React, { useState, useEffect} from "react";
 import { FormControl, List, TextField } from "@material-ui/core";
 import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
-import { TargetElement } from "@testing-library/user-event";
 import { makeStyles } from "@material-ui/styles";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import { db } from "./firebase";
+import { auth } from "./firebase";
 import TaskItem from "./TaskItem";
 import styles from "./App.module.css";
+import { AccessibleForwardTwoTone, AlternateEmailRounded } from "@material-ui/icons";
 
 const useStyles = makeStyles({
   field: {
@@ -19,10 +21,17 @@ const useStyles = makeStyles({
   },
 });
 
-const App: React.FC = () => {
+const App: React.FC = (props: any) => {
   const [tasks, setTasks] = useState([{id: "", title: ""}]);
   const [input, setInput] = useState("");
   const classess = useStyles();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((user) => {
+      !user && props.history.push("login");
+    });
+    return () => unSub();
+  });
 
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot) => {
@@ -42,6 +51,19 @@ const App: React.FC = () => {
   return ( 
     <div className={styles.app__root}>
       <h1>Todo App by React/Firebase</h1>
+
+      <button className={styles.app__logout}
+        onClick={
+          async () => {
+            try {await auth.signOut();
+              props.history.push("login");
+            } catch (error: any){
+              alert(error.message);
+            }
+          }}>
+            <ExitToAppIcon />
+      </button>
+
       <br />
       <FormControl>
         <TextField
